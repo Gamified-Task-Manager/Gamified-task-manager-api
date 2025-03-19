bb90c91e8f30673e0b15b45823f142cb47ce86fc
Terra Manning <terradmanning@gmail.com>
2025-03-19 17:33:27 -0600

    have the session token inside the user object

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,4 +1,3 @@
-# app/controllers/api/v1/sessions_controller.rb
 module Api
   module V1
     class SessionsController < ApplicationController
@@ -6,12 +5,14 @@ module Api
 
       def create
         user = User.find_by(email: params[:email])
+
         if user && user.authenticate(params[:password])
           token = AuthService.encode(user_id: user.id)
-          render json: {
-            data: UserSerializer.new(user).serializable_hash[:data],
-            token: token
-          }, status: :created
+
+          user_data = UserSerializer.new(user).serializable_hash[:data]
+          user_data[:attributes][:token] = token  # Clearly add token here
+
+          render json: { data: user_data }, status: :created
         else
           render json: ErrorMessageSerializer.serialize('Invalid email or password', 401), status: :unauthorized
         end
